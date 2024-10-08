a0b903b598ea643cac15dca14d720e842d8e3b6c
Igor <4430704@gmail.com>
2024-10-08 22:06:20 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,32 @@
+"use strict";
+const list = [
+    ...document.querySelectorAll("input")
+];
+const capitalizeFirstLetter = (string)=>{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+const transformName = (sourceName)=>{
+    // Split the sourceName by capital letters
+    const sourceNameArr = sourceName.split(/(?=[A-Z])/);
+    sourceNameArr.forEach((word, index)=>{
+        sourceNameArr[index] = capitalizeFirstLetter(word.toLowerCase());
+    });
+    return sourceNameArr.join(" ");
+};
+const createLabel = (input)=>{
+    const label = document.createElement("label");
+    label.textContent = transformName(input.name);
+    label.className = "field-label";
+    label.setAttribute("for", input.id);
+    input.insertAdjacentElement("beforebegin", label);
+};
+const setPlaceholder = (input)=>{
+    const inputName = transformName(input.name);
+    input.placeholder = capitalizeFirstLetter(inputName);
+};
+for (const input of list){
+    createLabel(input);
+    setPlaceholder(input);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
